47a96c0f93873c24b03ba601564eb33ffeba5cad
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2022-10-27 02:09:39 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,25 @@
-<?php include_once("views/index.html"); ?>
+<?php
 
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
 // Our web handlers
 
-$app->get('/home', function() use($app) {
+$app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('home.html');
+  return $app['twig']->render('index.html');
 });
+
+$app->run();
