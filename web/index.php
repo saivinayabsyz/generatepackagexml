ac6d9059afbf4d8cee6f8ff46ca46b7d06463939
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2022-10-27 02:07:55 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,2 +1,12 @@
 <?php include_once("views/index.html"); ?>
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/home', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('home.html');
+});
